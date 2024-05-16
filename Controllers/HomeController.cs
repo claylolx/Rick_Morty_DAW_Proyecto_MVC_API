6e8b8ca40b211abcc7c7b18fd4225885f30d069f
claylolx <145122199+claylolx@users.noreply.github.com>
2024-05-16 09:17:14 +0200

    Improvements and Starting point for naruto characters

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,47 +3,52 @@ using Microsoft.AspNetCore.Mvc;
 using PROJECTE_DAW_API.Models;
 using Newtonsoft.Json;
 
-namespace PROJECTE_DAW_API.Controllers;
-
-public class HomeController : Controller
+namespace PROJECTE_DAW_API.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Characters()
-    {
-        string rickandmortyApiUrl = "https://rickandmortyapi.com/api/character";
-        string rickandmortyApiResponse = GetApiResponse(rickandmortyApiUrl);
-        var characters = JsonConvert.DeserializeObject<Characters>(rickandmortyApiResponse).results;
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-        return View(characters);
-    }
+        public IActionResult Characters(int page = 1)
+        {
+            string rickandmortyApiUrl = $"https://rickandmortyapi.com/api/character?page={page}";
+            string rickandmortyApiResponse = GetApiResponse(rickandmortyApiUrl);
+            var charactersResponse = JsonConvert.DeserializeObject<Characters>(rickandmortyApiResponse);
 
+            ViewBag.NextPage = charactersResponse.info.next != null ? page + 1 : (int?)null;
+            ViewBag.PrevPage = charactersResponse.info.prev != null ? page - 1 : (int?)null;
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = charactersResponse.info.pages;
 
-    static string GetApiResponse(string url)
-    {
-        using HttpClient client = new HttpClient();
-        HttpResponseMessage response = client.GetAsync(url).Result;
-        return response.Content.ReadAsStringAsync().Result;
-    }
+            return View(charactersResponse.results);
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        static string GetApiResponse(string url)
+        {
+            using HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
